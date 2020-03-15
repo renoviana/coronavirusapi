af6a553fb5729de36a555f9b8c256d5136655c37
renoviana <renotviana@gmail.com>
2020-03-15 12:33:47 -0300

    make it better

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def conversor(arrayTd):
     }
 
 @app.route('/')
-def init(serie, rodada):
+def init():
     soup = BeautifulSoup(requests.get('https://www.worldometers.info/coronavirus').text, 'html.parser')
     coronaData = [conversor(tr.find_all('td') ) for tr in soup.find('table',{'id':'main_table_countries'}).find_all('tr')[1:]]
     return jsonify(coronaData)
